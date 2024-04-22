a2dd15f816ac356cd564179f279735b4a75a2c78
Slaven Konforta <slaven.konforta@gmail.com>
2024-04-22 20:10:15 +0200

    add zod validation

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -4,6 +4,7 @@ import { capitalize } from '@/lib/utils';
 import { Suspense } from 'react';
 import Loading from './loading';
 import { Metadata } from 'next';
+import { z } from 'zod';
 
 type MetadataProps = {
   params: {
@@ -22,15 +23,20 @@ export const generateMetadata = ({ params }: MetadataProps): Metadata => {
   };
 };
 
+const pageNumberSchema = z.coerce.number().int().positive().optional().default(1);
+
 export default async function EventsPage({ params, searchParams }: EventsPageProps) {
   const city = params.city;
-  const page = searchParams.page || 1;
+  const parsedPage = pageNumberSchema.safeParse(searchParams.page);
+  if (!parsedPage.success) {
+    throw new Error('Invalid page number');
+  }
 
   return (
     <main className='flex flex-col items-center px-[20px] py-24'>
       <H1 className='mb-28'>{city === 'all' ? 'All Events' : `Events in ${capitalize(city)}`}</H1>
-      <Suspense key={city + page} fallback={<Loading />}>
-        <EventsList city={city} page={+page} />
+      <Suspense key={city + parsedPage.data} fallback={<Loading />}>
+        <EventsList city={city} page={parsedPage.data} />
       </Suspense>
     </main>
   );
